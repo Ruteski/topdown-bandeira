ea4422e070a260fc04b8cc9c1f1fd6d2846231de
Lincoln Ruteski <lruteski@gmail.com>
2024-06-18 20:05:08 -0300

    criado animacoes de attack e codigo

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 class_name Player
 
 var _state_machine
+var _is_attacking: bool = false
 
 @export_category("Variables")
 @export var _move_speed: float = 64.0 # +/- 4 celulas por segundo
@@ -13,41 +14,59 @@ var _state_machine
 
 
 func _ready() -> void:
-	_state_machine = _animation_tree["parameters/playback"]
+		_state_machine = _animation_tree["parameters/playback"]
 
 	
 func _physics_process(_delta: float) -> void:
-	_move()
-	_animate()
-	move_and_slide()
+		_move()
+		_attack()
+		_animate()
+		move_and_slide()
 	
 	
 func _move() -> void:
-	var _direction: Vector2 = Vector2(
-		Input.get_axis("move_left", "move_right"),
-		Input.get_axis("move_up", "move_down")
-	)
+		var _direction: Vector2 = Vector2(
+				Input.get_axis("move_left", "move_right"),
+				Input.get_axis("move_up", "move_down")
+		)
 	
 #	if input_direction:
 #		velocity.x = input_direction * move_speed
 #	else:
 #		velocity.x = move_toward(velocity.x, 0, move_speed)
 
-	if _direction != Vector2.ZERO:
-		_animation_tree["parameters/idle/blend_position"] = _direction
-		_animation_tree["parameters/walk/blend_position"] = _direction
-
-		velocity.x = lerp(velocity.x, _direction.normalized().x * _move_speed, _acceleration)
-		velocity.y = lerp(velocity.y, _direction.normalized().y * _move_speed, _acceleration)
-		return
+		if _direction != Vector2.ZERO:
+				_animation_tree["parameters/idle/blend_position"] = _direction
+				_animation_tree["parameters/walk/blend_position"] = _direction
+				_animation_tree["parameters/attack/blend_position"] = _direction
+		
+				velocity.x = lerp(velocity.x, _direction.normalized().x * _move_speed, _acceleration)
+				velocity.y = lerp(velocity.y, _direction.normalized().y * _move_speed, _acceleration)
+				return
 	
-	velocity.x = lerp(velocity.x, _direction.normalized().x * _move_speed, _friction)
-	velocity.y = lerp(velocity.y, _direction.normalized().y * _move_speed, _friction)
-	velocity = _direction.normalized() * _move_speed
+		velocity.x = lerp(velocity.x, _direction.normalized().x * _move_speed, _friction)
+		velocity.y = lerp(velocity.y, _direction.normalized().y * _move_speed, _friction)
+		velocity = _direction.normalized() * _move_speed
 	
 	
 func _animate() -> void:
-	if velocity.length() > 2:
-		_state_machine.travel("walk")
-		return
-	_state_machine.travel("idle")
+		if _is_attacking:
+				_state_machine.travel("attack")
+				return
+	
+		if velocity.length() > 2:
+				_state_machine.travel("walk")
+				return
+		_state_machine.travel("idle")
+
+	
+func _attack() -> void:
+		if Input.is_action_just_pressed("attack") && !_is_attacking:
+				set_physics_process(false)
+				_is_attacking = true
+
+								
+func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
+		if anim_name == "attack_up" || anim_name == "attack_down" || anim_name == "attack_left" || anim_name == "attack_right":
+				_is_attacking = false
+				set_physics_process(true)
